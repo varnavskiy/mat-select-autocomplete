270c1aadab3228e1f5e3894aa775e9ae62ebf81f
David J <crono9977@users.noreply.github.com>
2020-03-09 18:41:44 -0400

    revert to angular/material 8 update

--- projects/select-autocomplete/src/lib/select-autocomplete.module.ts
+++ projects/select-autocomplete/src/lib/select-autocomplete.module.ts
@@ -4,8 +4,7 @@ import { MatIconModule } from '@angular/material/icon';
 import { MatSelectModule } from '@angular/material/select';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { SelectAutocompleteComponent } from './select-autocomplete.component';
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
